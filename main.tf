4c1c36cf31e9244d056161c9d1722948a17d6784
Mohamed Assaker <82214006+mohamedassaker@users.noreply.github.com>
2023-02-17 21:27:04 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -147,3 +147,15 @@ resource "aws_instance" "web-server-instance" {
         "Name" = "web-server"
     }
 }
+
+output "server_private_ip" {
+    value = aws_instance.web-server-instance.private_ip
+}
+
+output "server_public_ip" {
+    value = aws_instance.web-server-instance.public_ip
+}
+
+output "server_id" {
+    value = aws_instance.web-server-instance.id
+}
